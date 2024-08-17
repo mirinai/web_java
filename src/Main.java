aad87484adec85abce1ac78587a7f153ae491e1f
mirinai <154983447+mirinai@users.noreply.github.com>
2024-08-17 15:50:21 +0900

    basic

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//react -> app.tsx[엔트리 포인트]
+//java -> main[엔트리 포인트]
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+//    public static void main(String[] args) {
+//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//        // to see how IntelliJ IDEA suggests fixing it.
+////        System.out.printf("Hello and welcome!");
+//
+////        for (int i = 1; i <= 5; i++) {
+////            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+////            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+////            System.out.println("i = " + i);
+////        }
+//        System.out.println("ㄴㅇㄴㅇ");
+    public static void main(String[] args) {
+        System.out.println("hello"); //console.log()
+//        System.out.println("Main.main");
+//        System.out.println("args = " + Arrays.toString(args));
+//        System.out.println("args = " + args);
+//        System.out.println("Main.main");
+//        System.out.println("Main.main");
+    }
+
+}
